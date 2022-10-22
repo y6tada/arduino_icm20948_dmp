22deea65e3a8398c2099ad663f2ffb1073db8302
y6tada <y6tada.cnc@gmail.com>
2022-10-22 22:27:17 +0900

    check whoami register

--- arduino_icm20948_dmp/arduino_icm20948_dmp.ino
+++ arduino_icm20948_dmp/arduino_icm20948_dmp.ino
@@ -1,6 +1,33 @@
+#include <SPI.h>
+
+#define ICM20948_CS_PIN 12
+
 void setup() {
   // put your setup code here, to run once:
+  SPI.begin();
+  pinMode(ICM20948_CS_PIN, OUTPUT);
+  digitalWrite(ICM20948_CS_PIN, HIGH);
+  SPISettings _spisetting = SPISettings(7000000, MSBFIRST, SPI_MODE0);
+  
+  Serial.begin(115200);
 
+  while (!Serial);
+  Serial.println("program start");
+  
+  SPI.beginTransaction(_spisetting);
+  digitalWrite(ICM20948_CS_PIN, LOW);
+  uint8_t REG_SELECT_BANK0[] = {0x7F, 0x00};
+  Serial.println(SPI.transfer(127));
+  Serial.println(SPI.transfer(0));
+  digitalWrite(ICM20948_CS_PIN, HIGH);
+  SPI.endTransaction();
+
+  SPI.beginTransaction(_spisetting);
+  digitalWrite(ICM20948_CS_PIN, LOW);
+  Serial.println(SPI.transfer(0x00 | 0x80));
+  Serial.println(SPI.transfer(0x00));
+  digitalWrite(ICM20948_CS_PIN, HIGH);
+  SPI.endTransaction();
 }
 
 void loop() {
