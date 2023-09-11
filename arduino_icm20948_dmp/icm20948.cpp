a92585ed22333101138685b4de970454324d5073
Yusuke Tada <y6tada@Tada-MacBook-Air.local>
2023-09-10 21:40:23 -0700

    read_reg write_reg

--- arduino_icm20948_dmp/icm20948.cpp
+++ arduino_icm20948_dmp/icm20948.cpp
@@ -7,26 +7,36 @@ SPISettings icm20948_arduino_spi_setting = SPISettings(
     SPI_MODE0   // as datasheet
 );
 
-icm_state_t icm20948_init_spi(void)
+icm_state_t icm20948_init_spi(uint8_t _spi_cs_pin)
 {
     SPI.begin();
 
+    spi_cs_pin = _spi_cs_pin;
+
     // If you are using pre-implemented SPI CS pin on the arduino board, CS pin should be
     // automatically controlled so you don't need to control CS pin manually.
-    pinMode(ICM20948_CS_PIN, OUTPUT);
-    digitalWrite(ICM20948_CS_PIN, HIGH);
+    pinMode(spi_cs_pin, OUTPUT);
+    digitalWrite(spi_cs_pin, HIGH);
 
     return icm_state_no_error;
 }
 
 icm_state_t icm20948_write_reg(uint8_t addr, uint8_t *buf_write, uint16_t len_buf_write)
 {
+    // SPI Hardware Kickstart
     SPI.beginTransaction(icm20948_arduino_spi_setting);
-    digitalWrite(ICM20948_CS_PIN, LOW);
-    uint8_t REG_SELECT_BANK0[] = {0x7F, 0x00};
-    Serial.println(SPI.transfer(127));
-    Serial.println(SPI.transfer(0));
-    digitalWrite(ICM20948_CS_PIN, HIGH);
+    SPI.transfer(0x00);
+    SPI.endTransaction();
+
+    SPI.beginTransaction(icm20948_arduino_spi_setting);
+    digitalWrite(spi_cs_pin, LOW);
+
+    SPI.transfer(((addr & 0x7F) | 0x00));
+    for (int i = 0; i < len_buf_write; ++i)
+    {
+        SPI.transfer(*(buf_write + i));
+    }
+    digitalWrite(spi_cs_pin, HIGH);
     SPI.endTransaction();
 
     return icm_state_no_error;
@@ -34,24 +44,21 @@ icm_state_t icm20948_write_reg(uint8_t addr, uint8_t *buf_write, uint16_t len_bu
 
 icm_state_t icm20948_read_reg (uint8_t addr, uint8_t *buf_read, uint16_t len_buf_read)
 {
+    // SPI Hardware Kickstart
     SPI.beginTransaction(icm20948_arduino_spi_setting);
-    digitalWrite(ICM20948_CS_PIN, LOW);
-    Serial.println(SPI.transfer(addr | 0x80));
-    while (1)
+    SPI.transfer(0x00);
+    SPI.endTransaction();
+
+    SPI.beginTransaction(icm20948_arduino_spi_setting);
+    digitalWrite(spi_cs_pin, LOW);
+    
+    SPI.transfer(((addr & 0x7F) | 0x80));
+    for (int i = 0; i < len_buf_read; ++i)
     {
-        if (len_buf_read == 0)
-        {
-            break;
-        }
-        --len_buf_read;
-        *buf_read = SPI.transfer(0x00);
-        ++buf_read;
+        *(buf_read + i) = SPI.transfer(0x00);
     }
     
-    
-    Serial.println(SPI.transfer(addr | 0x80));
-    Serial.println(SPI.transfer(0x00));
-    digitalWrite(ICM20948_CS_PIN, HIGH);
+    digitalWrite(spi_cs_pin, HIGH);
     SPI.endTransaction();
 
     return icm_state_no_error;
--- arduino_icm20948_dmp/icm20948.h
+++ arduino_icm20948_dmp/icm20948.h
@@ -12,9 +12,11 @@ typedef enum
 
 #define ICM20948_CS_PIN 12
 
-extern icm_state_t icm20948_init_spi(void);
+extern icm_state_t icm20948_init_spi(uint8_t _spi_ss_pin);
 extern icm_state_t icm20948_select_bank(uint8_t bank);
 extern icm_state_t icm20948_write_reg(uint8_t addr, uint8_t *buf_write, uint16_t len_buf_write);
 extern icm_state_t icm20948_read_reg (uint8_t addr, uint8_t *buf_read, uint16_t len_buf_read);
+
+static uint8_t spi_cs_pin = 0;
 
 #endif /* __ICM20948_H__ */
